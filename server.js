d6741f436bf5baf7b6f90dce617790ef76229177
Aman Mishra <amanm4345@gmail.com>
2024-05-08 16:23:14 +0530

    EAT: Set up Tailwind CSS CDN file (v1.0).

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3300;
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
 
+//Assets
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
+//Set template engine
+app.use(expressLayout);
+app.set('views',path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
 
 //Start the server
 app.listen(PORT, () => {
