9837076707011d73f0e395cd16a7af2c7b6d54ee
Aman Mishra <amanm4345@gmail.com>
2024-05-09 16:38:37 +0530

    FEAT: Static page added for Cart items.

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+
+//Routes for cart page
+app.get('/cart', (req, res) => {
+    res.render('customer/cart');
+})
+
 //Start the server
 app.listen(PORT, () => {
     console.log(`Server listening on localhost:${PORT}`);
