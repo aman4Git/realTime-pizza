c0c4bfa6046c24bc41f0331947e58c533a46407a
Aman Mishra <amanm4345@gmail.com>
2024-05-15 15:25:23 +0530

    FEAT: User registration functionality added.

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,15 +1,80 @@
+const User = require('../../models/user');
+const bcrypt = require('bcrypt')
+
 function authController()
 {
     return {
+
+        //Function to handle & render Register page.
+        register(req, res){
+            res.render('auth/register')
+        },
+
+        //Add a new user
+        async registerUser(req, res){
+
+            //req body data
+            const { name, email, password } = req.body;
+
+            //validate required
+            if(!name || !email || !password){
+
+                //flash message
+                req.flash('error', 'All fields are required');
+                req.flash('name', name);
+                req.flash('email', email);
+
+                //redirect to register page
+                return res.redirect('/register');
+            }
+
+            //Check if email is already registered
+            const emailExists = await User.exists({ email: email });
+
+                if(emailExists){
+
+                    //flash message
+                    req.flash('error', 'Email is already registered');
+                    req.flash('name', name);
+                    req.flash('email', email);
+
+                    //redirect to register page
+                    return res.redirect('/register');
+                }
+
+            //Hash password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            //Create a new user
+            const user = new User({
+                name,
+                email,
+                password: hashedPassword
+            });
+
+            //Save user to database
+            user.save().then((user) =>{
+
+                //redirect to home page
+                return res.redirect('/');
+                
+            }).catch(err =>{
+
+                //flash message
+                req.flash('error', 'Something went wrong');
+
+                //redirect to register page
+                return res.redirect('/register');
+                
+            });
+
+        },
+
         //Function to handle & render Login page.
         login(req, res){
             res.render('auth/login')
         },
 
-        //Function to handle & render Register page.
-        register(req, res){
-            res.render('auth/register')
-        }
     }
 }
 
--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.use(flash());
 
 //Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 //Global middleware
