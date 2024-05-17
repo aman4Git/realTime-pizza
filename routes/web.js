19f8ec4679116ef25d61361a1c60bb268ab52e22
Aman Mishra <amanm4345@gmail.com>
2024-05-17 16:13:43 +0530

    FIX: authMiddleware name changed to guestMiddleware.

--- routes/web.js
+++ routes/web.js
@@ -2,7 +2,7 @@ const homeController = require('../app/http/controllers/homeController');
 const authController = require('../app/http/controllers/authController');
 const cartController = require('../app/http/controllers/customers/cartController');
 const orderController = require('../app/http/controllers/customers/orderController');
-const authMiddleware = require('../app/http/middleware/auth');
+const guestMiddleware = require('../app/http/middleware/guest');
 
 function initRoutes(app){
 
@@ -10,9 +10,9 @@ function initRoutes(app){
     app.get('/', homeController().index);
 
     //Auth related routes
-    app.get('/register', authMiddleware, authController().register);
+    app.get('/register', guestMiddleware, authController().register);
     app.post('/register', authController().registerUser);
-    app.get('/login', authMiddleware, authController().login);
+    app.get('/login', guestMiddleware, authController().login);
     app.post('/login', authController().userLogin);
     app.post('/logout', authController().logout);
 
