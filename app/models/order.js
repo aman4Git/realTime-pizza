f37ef8eeb6225e902aedc72af4881d0d4f255aa3
Aman Mishra <amanm4345@gmail.com>
2024-05-17 18:48:58 +0530

    FIX: Minor fix.

--- app/models/order.js
+++ app/models/order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    customerId : {type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true},
+    customerId : {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     items      : {type: Object, required: true},
     phone      : {type: String, required: true},
     address    : {type: String, required: true},
